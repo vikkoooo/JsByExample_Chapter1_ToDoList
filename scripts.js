ec869100f40dcda8b18bca30b5b20c3b044496c8
vikkoooo <vikkolundberg@gmail.com>
2023-09-08 15:24:35 +0200

    Template literals
    completed

---
+++ scripts.js
@@ -0,0 +1,40 @@
+class ToDoClass {
+	constructor() {
+		this.tasks = [
+			{ task: "Go to dentist", isComplete: false },
+			{ task: "Do gardening", isComplete: true },
+			{ task: "Renew library account", isComplete: false }
+		];
+		this.loadTasks();
+	}
+	loadTasks() {
+		let tasksHtml = this.tasks.reduce((html, task, index) => html += this.generateTaskHtml(task, index), '');
+		document.getElementById('taskList').innerHTML = tasksHtml;
+	}
+
+
+	generateTaskHtml(task, index) {
+		return `
+		<li class="list-group-item checkbox"> 
+			<div class="row">
+				<div class="col-md-1 col-xs-1 col-lg-1 col-sm-1 checkbox"> 
+					<label><input id="toggleTaskStatus" type="checkbox" onchange="toDo.toggleTaskStatus(${index})" value="" class="" ${task.isComplete ? 'checked' : ''}></label>
+				</div>
+				<div class="col-md-10 col-xs-10 col-lg-10 col-sm-10 task-text ${task.isComplete ? 'complete' : ''}">${task.task} 
+				</div>
+				<div class="col-md-1 col-xs-1 col-lg-1 col-sm-1 delete-icon-area"> 
+					<a class="" href="/" onClick="toDo.deleteTask(event, ${index})"><i id="deleteTask" data-id="${index}" class="delete-icon glyphicon glyphicon-trash"></i></a>
+				</div> 
+			</div>
+	</li> `;
+	}
+
+}
+
+
+
+// When page is loaded, create new ToDoClass.
+let toDo;
+window.addEventListener("load", () => {
+	toDo = new ToDoClass;
+});
