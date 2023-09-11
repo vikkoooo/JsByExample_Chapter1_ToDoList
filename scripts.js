5fc0f59f9c492c3e9c598a40f62df29fad3e1604
vikkoooo <vikkolundberg@gmail.com>
2023-09-11 15:44:49 +0200

    managing and adding tasks

--- scripts.js
+++ scripts.js
@@ -12,7 +12,6 @@ class ToDoClass {
 		document.getElementById('taskList').innerHTML = tasksHtml;
 	}
 
-
 	generateTaskHtml(task, index) {
 		return `
 		<li class="list-group-item checkbox"> 
@@ -28,6 +27,40 @@ class ToDoClass {
 			</div>
 	</li> `;
 	}
+
+	toggleTaskStatus(index) {
+		this.tasks[index].isComplete = !this.tasks[index].isComplete;
+		this.loadTasks();
+	}
+
+	deleteTask(event, taskIndex) {
+		event.preventDefault();
+		this.tasks.splice(taskIndex, 1);
+		this.loadTasks();
+	}
+
+	addTaskClick() {
+		let target = document.getElementById("addTask");
+		this.addTask(target.value);
+		target.value = "";
+	}
+
+	addTask(task) {
+		let newTask = { task, isComplete: false, };
+		let parentDiv = document.getElementById("addTask").parentElement;
+
+		if (task === "") {
+			parentDiv.classList.add("has-error");
+		}
+		else {
+			parentDiv.classList.remove("has-error");
+			this.tasks.push(newTask);
+			this.loadTasks();
+		}
+
+	}
+
+
 
 }
 
